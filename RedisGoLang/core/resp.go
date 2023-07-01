9967e676454cc78d9ad6edef6afe75ade5a502cc
epN-nirmalgeo <nirmalgeo@gmail.com>
2023-07-01 10:04:16 -0700

    some changes

--- RedisGoLang/core/resp.go
+++ RedisGoLang/core/resp.go
@@ -55,7 +55,7 @@ func decodeInt64(data []byte) (int64, int, error) {
 		panic(err)
 	}
 
-	return value, pos, nil
+	return value, pos + 2, nil
 }
 
 func decodeBulkString(data []byte) (string, int, error) {
@@ -73,7 +73,44 @@ func decodeBulkString(data []byte) (string, int, error) {
 		lenBulkString = lenBulkString*10 + int(data[pos]-'0')
 	}
 
-	//pos
+	pos += 2
+	var start_pos = pos
+	for ; data[pos] != '\r'; pos++ {
+	}
+
+	return string(data[start_pos:pos]), pos + 2, nil
+}
+
+func DecodeArray(data []byte) ([]interface{}, int, error) {
+
+	if len(data) == 0 {
+		return nil, 0, nil
+	}
+
+	if data[0] != '*' {
+		panic("not an array")
+	}
+
+	pos := 1
+	var cnt int64 = 0
+	for ;data[pos] != '\r'; pos++ {
+		cnt = cnt * 10 + int64(data[pos]-'0')
+	}
+
+	pos += 2
+
+	var arrayElements [] interface{} = make([]interface{}, cnt)
+
+	for i := range arrayElements {
+		value, delta, err := DecodeBytes(data[pos:])
+		if err != nil {
+			panic(err)
+		}
+		pos += delta
+		arrayElements[i] = value
+	}
+
+	return arrayElements, pos+2, nil
 }
 
 // DecodeBytes : Decode bytes using the RESP protocol. Returns the data,
@@ -91,8 +128,14 @@ func DecodeBytes(data []byte) (interface{}, int, error) {
 		return decodeErrorString(data)
 	case ':':
 		return decodeInt64(data)
-
+	case '$':
+		return decodeBulkString(data)
 	}
 
 	return nil, 0, nil
+}
+
+
+func DecodeSelection(data []byte) {
+
 }
--- RedisGoLang/core/resp_test.go
+++ RedisGoLang/core/resp_test.go
@@ -4,6 +4,7 @@ import (
 	"RedisGoLang/core"
 	"log"
 	"testing"
+	"fmt"
 )
 
 func TestDecodeSimpleString(t *testing.T) {
@@ -28,5 +29,40 @@ func TestDecodeInt64(t *testing.T) {
 	if err != nil || value != int64(1231232131232) {
 		log.Println(value)
 		t.Fail()
+	}
+}
+
+func TestBulkString(t *testing.T) {
+	var bulkString = "$4\r\nTEST\r\n"
+	value, _, err := core.DecodeBytes([]byte(bulkString))
+	if err != nil || value != "TEST" {
+		log.Println(value)
+		t.Fail()
+	}
+}
+
+func TestArray(t *testing.T) {
+	cases := map[string][]interface{}{
+		"*0\r\n":                                                   {},
+		"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n":                     {"hello", "world"},
+		"*3\r\n:1\r\n:2\r\n:3\r\n":                                 {int64(1), int64(2), int64(3)},
+		"*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n":            {int64(1), int64(2), int64(3), int64(4), "hello"},
+		"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n": {[]int64{int64(1), int64(2), int64(3)}, []interface{}{"Hello", "World"}},
+	}
+	for k, v := range cases {
+		value, _ , err := core.DecodeArray([]byte(k))
+		if err != nil {
+			log.Println(err)
+			t.Fail()
+		}
+		array := value.([]interface{})
+		if len(array) != len(v) {
+			t.Fail()
+		}
+		for i := range array {
+			if fmt.Sprintf("%v", v[i]) != fmt.Sprintf("%v", array[i]) {
+				t.Fail()
+			}
+		}
 	}
 }
